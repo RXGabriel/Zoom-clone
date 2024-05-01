1f37d96095e1db99a66ab197403156bcccc36651
RXGabriel <gabriel2005rf@gmail.com>
2024-05-01 12:04:37 -0300

    feat: Add recording functionality and view initialization

---
+++ public/pages/room/src/app.js
@@ -0,0 +1,17 @@
+const recordClick = function (recorderBtn) {
+  this.recordingEnabled = false;
+  return () => {
+    this.recordingEnabled = !this.recordingEnabled;
+    recorderBtn.style.color = this.recordingEnabled ? "red" : "white";
+  };
+};
+
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get("room");
+  console.log("this is the room", room);
+
+  const view = new View();
+};
+
+window.onload = onload;
