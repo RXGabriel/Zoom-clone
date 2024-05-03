20486d64a5559c58069f1bbfd6cd1ffa5177fb23
RXGabriel <gabriel2005rf@gmail.com>
2024-05-03 12:00:40 -0300

    feat: Add  _setup method to initialize recording options in Recorder class

--- public/pages/room/src/Util/recorder.js
+++ public/pages/room/src/Util/recorder.js
@@ -5,6 +5,21 @@ class Recorder {
     this.filename = `id:${userName}-when:${Date.now()}`;
   }
 
+  _setup() {
+    const commonCodecs = ["codecs=vp9,opus", "codecs=vp8,opus", ""];
+    const options = commonCodecs
+      .map((codec) => ({ mimeType: `${this.videoType};${codec}` }))
+      .find((options) => MediaRecorder.isTypeSupported(options.mimeType));
+
+    if (!options) {
+      throw new Error(
+        `none of the codecs: ${commonCodecs.join(",")} are supported`
+      );
+    }
+
+    return options;
+  }
+
   startRecording() {}
   stopRecording() {}
 }
