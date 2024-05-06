a906636eb17d6f6d178d11b357066841fa02a0a2
RXGabriel <gabriel2005rf@gmail.com>
2024-05-06 10:20:56 -0300

    feat: Add OnLeaveClick and configureLeaveButton methods to Business class

--- public/pages/room/src/view.js
+++ public/pages/room/src/view.js
@@ -1,6 +1,7 @@
 class View {
   constructor() {
     this.recorderBtn = document.getElementById("record");
+    this.leaveBtn = document.getElementById("leave");
   }
 
   createVideoElement({ muted = true, src, srcObject }) {
@@ -73,7 +74,19 @@ class View {
     };
   }
 
+  OnLeaveClick(command) {
+    return async () => {
+      command();
+      await Util.sleep(4000);
+      window.location = "/pages/home";
+    };
+  }
+
   configureRecordButton(command) {
     this.recorderBtn.addEventListener("click", this.OnRecordClick(command));
+  }
+
+  configureLeaveButton(command) {
+    this.leaveBtn.addEventListener("click", this.OnLeaveClick(command));
   }
 }
